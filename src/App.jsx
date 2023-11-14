e139e2e5d67bf35af4b239c618be8b6b557cbe04
Tur234567 <facktick1910@mail.ru>
2023-11-14 21:14:20 +0500

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import { AppRoutes } from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import { NewLogin } from './components/newLogin/newLogin';
-
+// for pull request
 function App() {
   return (
     <BrowserRouter>
