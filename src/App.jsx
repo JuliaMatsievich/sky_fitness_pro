f2c8d0543272e323a1f265d6a4131423e96102d3
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-15 12:15:25 +0500

    перименование файлов с js на jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
 import { AppRoutes } from './routes';
-// import { MainPage } from './pages/main/MainPage';
 
 function App() {
   return (
     <div>
-      {/* <p>SkyFitnessPro</p> */}
       <AppRoutes />
     </div>
   );
