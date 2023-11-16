4d42c2c0301baf9bc92a693958da652cbcda7d5e
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-16 14:18:28 +0500

    сделала линк с карточек на стр. курса

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './card.css'
 import { cards } from '../../constants/cardsConst'
+import { Link } from 'react-router-dom'
 
 export const Cards = () => {
 
   return (
     <>
       {cards.map((card) => (
-        <div className="card"  key={card.id}>
+        <Link to='/curs'  key={card.id}>
+        <div className="card" >
           <span>{card.title}</span>
           <img className="card-img" src={card.src} alt="" />
         </div>
+        </Link>
       ))}
     </>
   )
