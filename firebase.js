624e0a27571c0280db0d678bbb4ac8b4912986ef
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-20 15:24:54 +0500

    init firebase

--- firebase.js
+++ firebase.js
@@ -5,15 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBv29vdBXHCImJPcK4nKNVef1bI_OC4F54",
-  authDomain: "sky-fitness-pro-89bf6.firebaseapp.com",
-  databaseURL: "https://sky-fitness-pro-89bf6-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sky-fitness-pro-89bf6",
-  storageBucket: "sky-fitness-pro-89bf6.appspot.com",
-  messagingSenderId: "945963956005",
-  appId: "1:945963956005:web:076422c75407d67dfd0ccc"
+  apiKey: "AIzaSyDqa4cOzW_N8OKaxBptQb_PqSBYbP0XN6U",
+  authDomain: "sky-fitness-pro-83d20.firebaseapp.com",
+  databaseURL: "https://sky-fitness-pro-83d20-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sky-fitness-pro-83d20",
+  storageBucket: "sky-fitness-pro-83d20.appspot.com",
+  messagingSenderId: "718553486746",
+  appId: "1:718553486746:web:b0ee9323b4716e89751623"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
