f8149a74e9aa08bdbd7110d721e05971ed6026b2
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-20 16:05:26 +0500

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
 import { AppRoutes } from './routes'
 import { createContext, useState } from 'react'
-
+import { firestore } from './firebase'
 export const UserContext = createContext(null)
 
 function App() {
   const [isUser, setIsUser] = useState(true)
 
+  console.log(firestore.collection('courses').get())
+
   return (
     <>
-        <UserContext.Provider
-          value={{isUser, setIsUser}}
-        >
-          <AppRoutes />
-        </UserContext.Provider>
+      <UserContext.Provider value={{ isUser, setIsUser }}>
+        <AppRoutes />
+      </UserContext.Provider>
     </>
   )
 }
