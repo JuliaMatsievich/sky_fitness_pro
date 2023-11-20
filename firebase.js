528c474f9ed506d22ac3983cbe001cf4b6568466
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-20 14:53:30 +0500

    init firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBv29vdBXHCImJPcK4nKNVef1bI_OC4F54",
+  authDomain: "sky-fitness-pro-89bf6.firebaseapp.com",
+  databaseURL: "https://sky-fitness-pro-89bf6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sky-fitness-pro-89bf6",
+  storageBucket: "sky-fitness-pro-89bf6.appspot.com",
+  messagingSenderId: "945963956005",
+  appId: "1:945963956005:web:076422c75407d67dfd0ccc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
