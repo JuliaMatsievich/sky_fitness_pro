4cb7762cf3f82b8616c81ea12324b2a4f36fcd97
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-20 19:53:10 +0500

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { AppRoutes } from './routes'
 import { createContext, useState } from 'react'
-import { firestore } from './firebase'
+// import { firestore } from './firebase'
 export const UserContext = createContext(null)
 
 function App() {
   const [isUser, setIsUser] = useState(true)
 
-  console.log(firestore.collection('courses').get())
+  // console.log(firestore.collection('courses').get())
 
   return (
     <>
