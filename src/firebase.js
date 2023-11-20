9d158914971cafba1b888be450e6163286162b94
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-20 23:35:01 +0500

    db

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import 'firebase/firestore';
+import { getDatabase } from "firebase/database";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,3 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore();
+export const database = getDatabase(app);
